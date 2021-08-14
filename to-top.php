6080b528476b1d3528bf833440478cf6643b70fb
Adam Kayce <brightcoconut@gmail.com>
2021-08-14 14:11:40 -0500

    Add files via upload

---
+++ to-top.php
@@ -0,0 +1,13 @@
+<?php //* don't include
+
+// Enqueue To Top script
+add_action( 'wp_enqueue_scripts', 'to_top_script' );
+function to_top_script() {
+    wp_enqueue_script( 'to-top', '/wp-content/plugins/bright-coconut/to-top.js', array( 'jquery' ), '1.0', true );
+}
+
+// Add To Top button
+add_action( 'genesis_before', 'genesis_to_top');
+	function genesis_to_top() {
+	 echo '<a href="#0" class="to-top" title="Back To Top">Top</a>';
+}
